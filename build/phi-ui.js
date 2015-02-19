7a21196e135327e03e64be01eaed5d54c2acdb95
Santiago Cortes <santiago.cortes@phidias.com.co>
2015-02-19 13:40:22 -0500

    refactored phi-select to use ngModelController and implemented <phi-option> element

--- build/phi-ui.js
+++ build/phi-ui.js
@@ -1,11 +1,7 @@
-angular.module("phi.ui", ['ngAria']);
+angular.module("phi.ui", ["ngAria"]);
 
 angular.module('phi.ui').run(["$rootScope", "$location", function($rootScope, $location) {
-
 	$rootScope.$location = $location;
-
-	$rootScope.cities = ['cyan', 'default', 'amber', 'blue-grey', 'blue', 'brown', 'deep-orange', 'deep-purple', 'green', 'grey', 'indigo', 'light-blue', 'light-green', 'lime', 'orange', 'pink', 'purple', 'red', 'teal', 'yellow'];
-
 }]);
 angular.module("phi.ui").service("$phiCoordinates", ["$timeout", function($timeout) {
 
@@ -889,21 +885,22 @@ angular.module("phi.ui").directive("phiSubmenu", [function() {
 }]);
 
 /*
-Same attributes as polymer's paper-element
+Based on http://www.bennadel.com/blog/2756-experimenting-with-ngmodel-and-ngmodelcontroller-in-angularjs.htm
 */
 
-angular.module("phi.ui").directive("phiSelect", ["$compile", "$document", function($compile, $document) {
+angular.module("phi.ui").directive("phiSelect", ["$compile", "$document", "$timeout", function($compile, $document, $timeout) {
 
     var phiSelectCounter = 1;
 
     return {
+
         restrict: "E",
+
+        require: "?ngModel",
 
         scope: {
             name:     "@",
             label:    "@",
-            ngModel:  "=",
-            ngChange: "&",
             ngFocus:  "&",
             ngBlur:   "&",
             onSearch: "&phiOnSearch"
@@ -912,33 +909,83 @@ angular.module("phi.ui").directive("phiSelect", ["$compile", "$document", functi
 
         transclude: true,
 
-        template:  '<phi-input id="{{elementId}}" label="{{label}}" ng-model="query" tabindex="-1" ng-focus="expand()"></phi-input>' +
+        template:  '<div class="phi-select-header" id="{{elementId}}">' +
+                       '<div ng-show="!state.expanded" class="phi-select-display" ng-click="expand()"></div>' +
+                       //'<phi-input ng-show="state.expanded" label="{{label}}" ng-model="query" tabindex="-1"></phi-input>' +
+                       '<input type="text" ng-show="state.expanded" ng-model="query" tabindex="-1" size="2" />' +
+                   '</div>' +
                    '<phi-menu ng-transclude phi-tooltip-for="{{elementId}}" phi-tooltip-match="width" phi-visible="{{state.expanded}}" class="phi-visible-slide-bottom phi-texture-paper"></phi-menu>',
 
-        controller: ["$scope", "$element", function($scope, $element) {
+        controller: ["$scope", function($scope) {
 
-            this.select = function(value, element) {
-                $scope.ngModel = value;
-                $scope.collapse();
-                $scope.ngChange();
-            };
+            this.handleOptionClick = function(element) {
+                return $scope.handleOptionClick(element);
+            }
 
         }],
 
 
-        link: function(scope, element, attributes) {
+        link: function(scope, element, attributes, ngModelController) {
 
-            element.attr("tabindex", -1);
-
+            element.attr("tabindex", 0);
             scope.elementId = "phi-select-" + phiSelectCounter++;
             element.data("phiSelectId", scope.elementId);
 
-            scope.query = null;
+            element.on("focus", function() {
+                scope.expand();
+                scope.$apply();
+            });
 
+
+            var displayElement = angular.element(element.find('div')[1]);
+
+            ngModelController.$render = function() {
+                scope.$evalAsync( renderSelectedOption );
+            };
+
+            renderSelectedOption = function() {
+
+                var option = findOptionWithValue(ngModelController.$viewValue);
+
+                if ( option ) {
+                    displayElement.html( option.html() );
+                }
+
+            };
+
+
+            findOptionWithValue = function(value) {
+
+                var options = element.find('phi-option');
+
+                for ( var i = 0; i < options.length; i++ ) {
+
+                    var option = angular.element( options[i] );
+
+                    if ( option.attr("value") == value ) {
+                        return option;
+                    }
+
+                }
+
+                return null;
+
+            };
+
+
+            scope.query = null;
             scope.state = {
                 expanded: false
             };
 
+
+            scope.handleOptionClick = function(option) {
+
+                ngModelController.$setViewValue(option.attr("value"));
+                renderSelectedOption();
+                scope.collapse();
+
+            };
 
             scope.expand = function() {
 
@@ -947,18 +994,25 @@ angular.module("phi.ui").directive("phiSelect", ["$compile", "$document", functi
                 }
 
                 scope.query = '';
-
                 scope.state.expanded = true;
-                $document.bind('click', scope.documentClicked);
+
+
+                $timeout(function() {
+                    element.find("input")[0].focus();
+                }, 0)
+
+
+
+                $document.bind('click', handleDocumentClick);
             };
 
             scope.collapse = function() {
                 scope.state.expanded = false;
                 scope.query          = scope.ngModel;
-                $document.unbind('click', scope.documentClicked);
+                $document.unbind('click', handleDocumentClick);
             };
 
-            scope.documentClicked = function(e) {
+            handleDocumentClick = function(e) {
 
                 if (angular.element(e.target).inheritedData('phiSelectId') == scope.elementId) {
                     return;
@@ -978,10 +1032,6 @@ angular.module("phi.ui").directive("phiSelect", ["$compile", "$document", functi
             });
 
 
-            scope.$watch("ngModel", function(newValue, oldValue) {
-                scope.query = newValue;
-            });
-
         }
 
     };
@@ -989,28 +1039,19 @@ angular.module("phi.ui").directive("phiSelect", ["$compile", "$document", functi
 }]);
 
 
-angular.module("phi.ui").directive("option", ["$compile", "$interpolate", function($compile, $interpolate) {
+angular.module("phi.ui").directive("phiOption", ["$compile", "$interpolate", function($compile, $interpolate) {
 
     return {
-        restrict: "E",
-        require:  "^?phiSelect",
-
-        scope: {
-            value: "="
-        },
-
+        restrict:   "E",
+        require:    "^phiSelect",
         template:   '<a ng-click="select()" ng-transclude></a>',
         transclude: true,
-        replace:    true,
+        scope:      {},
 
         link: function(scope, element, attributes, phiSelectCtrl) {
 
-            if (!phiSelectCtrl) {
-                return;
-            }
-
             scope.select = function() {
-                return phiSelectCtrl.select(scope.value, element);
+                return phiSelectCtrl.handleOptionClick(element);
             };
 
         }
--- src/core/module.js
+++ src/core/module.js
@@ -1,9 +1,5 @@
-angular.module("phi.ui", ['ngAria']);
+angular.module("phi.ui", ["ngAria"]);
 
 angular.module('phi.ui').run(["$rootScope", "$location", function($rootScope, $location) {
-
 	$rootScope.$location = $location;
-
-	$rootScope.cities = ['cyan', 'default', 'amber', 'blue-grey', 'blue', 'brown', 'deep-orange', 'deep-purple', 'green', 'grey', 'indigo', 'light-blue', 'light-green', 'lime', 'orange', 'pink', 'purple', 'red', 'teal', 'yellow'];
-
 }]);
