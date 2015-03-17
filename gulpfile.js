7f973680879e38d94dcedc86022983a606876726
Santiago Cortes <santiago.cortes@phidias.com.co>
2015-03-17 10:45:29 -0500

    configurable gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,9 @@
+//Global installation
+//npm install gulp gulp-concat gulp-rename gulp-sass gulp-minify-css gulp-uglify 
+
+// Set the full path to the folder (only necessary when running gulp outside the project folder)
+var basedir = "";
+
 // Include gulp
 var gulp = require('gulp');
 
@@ -16,31 +22,31 @@ function logError(error) {
 
 // Compile and minify Sass
 gulp.task('sass', function() {
-    return gulp.src(['src/core/styles/normalize/*.scss', 'src/core/styles/mixins/**/*.scss', 'src/components/**/*.scss'])
+    return gulp.src([basedir+'src/core/styles/normalize/*.scss', basedir+'src/core/styles/mixins/**/*.scss', basedir+'src/components/**/*.scss'])
     	.pipe(concat('phi-ui.css'))
         .pipe(sass())
             .on('error', logError)
-        .pipe(gulp.dest('build'))
+        .pipe(gulp.dest(basedir+'build'))
         .pipe(rename('phi-ui.min.css'))
         .pipe(minifyCSS())
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest(basedir+'build'));
 });
 
 // Concatenate and minify javascript
 gulp.task('js', function() {
-    return gulp.src(['src/core/**/*.js', 'src/components/**/*.js'])
+    return gulp.src([basedir+'src/core/**/*.js', basedir+'src/components/**/*.js'])
         .pipe(concat('phi-ui.js'))
-        .pipe(gulp.dest('build'))
+        .pipe(gulp.dest(basedir+'build'))
         .pipe(rename('phi-ui.min.js'))
         .pipe(uglify())
             .on('error', logError)
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest(basedir+'build'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.scss', ['sass']);
-    gulp.watch('src/**/*.js', ['js']);
+    gulp.watch(basedir+'src/**/*.scss', ['sass']);
+    gulp.watch(basedir+'src/**/*.js', ['js']);
 });
 
 // Fly!
